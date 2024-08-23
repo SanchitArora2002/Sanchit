5008806c6053a770dea948b7138ebeb2f2bf70a0
Sanchit Arora <140871285+SanchitArora2002@users.noreply.github.com>
2024-08-23 11:24:18 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// (function () {
+//     [...document.querySelectorAll(".control")].forEach(button => {
+//         button.addEventListener("click", function() {
+//             document.querySelector(".active-btn").classList.remove("active-btn");
+//             this.classList.add("active-btn");
+//             document.querySelector(".active").classList.remove("active");
+//             document.getElementById(button.dataset.id).classList.add("active");
+//         })
+//     });
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("dark-mode");
+//     })
+// })();
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        const profileImage = document.getElementById("profile-image");
+        if (document.body.classList.contains("dark-mode")) {
+            profileImage.src = "2.PNG"; // Dark mode image
+        } else {
+            profileImage.src = "1.PNG"; // Light mode image
+        }
+    });
+})();
+
+// function flipCard(element) {
+//     element.classList.toggle('flip');
+// }
